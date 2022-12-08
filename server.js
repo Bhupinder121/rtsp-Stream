deb01ca02d929ab48b555496b2e9228f43c8c86f
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2022-12-08 18:07:57 +0530

    jsmpeg added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 let Stream = require('node-rtsp-stream');
-let s = "'s"
+
 let stream1 = new Stream({
-    name: "testing",
+    name: "nvr",
     streamUrl: `rtsp://admin:Jaspal's@192.168.0.100:554/Streaming/Channels/101`,
-    wsPort: 99, 
+    wsPort: 90, 
     ffmpegOptions: { // options ffmpeg flags
         '-stats': '', // an option with no neccessary value uses a blank string
         '-r': 30 // options with required values specify the value after the key
@@ -11,3 +11,12 @@ let stream1 = new Stream({
 });
 
 
+let stream2 = new Stream({
+    name: "camera",
+    streamUrl: `rtsp://admin:jp@12345@192.168.0.103:554/Streaming/Channels/101`,
+    wsPort: 91, 
+    ffmpegOptions: { // options ffmpeg flags
+        '-stats': '', // an option with no neccessary value uses a blank string
+        '-r': 30 // options with required values specify the value after the key
+    }
+});
