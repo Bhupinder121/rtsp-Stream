865621541f8ce544a5b29d537895a423f3abc085
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2022-12-09 13:27:21 +0530

    something

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ let s = "'s"
 let stream1 = new Stream({
     name: "testing",
     streamUrl: `rtsp://admin:Jaspal's@192.168.0.100:554/Streaming/Channels/101`,
-    wsPort: 99, 
+    wsPort: 90, 
     ffmpegOptions: { // options ffmpeg flags
         '-stats': '', // an option with no neccessary value uses a blank string
         '-r': 30 // options with required values specify the value after the key
     }
 });
+
 
 
